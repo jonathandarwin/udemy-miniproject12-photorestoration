149db30fd979674147be21da2544994be91aafd1
jonathandarwin <jonathanjojo38@yahoo.com>
2019-06-16 13:22:25 +0700

    add code explanation

--- 12_photo_restoration.py
+++ 12_photo_restoration.py
@@ -2,30 +2,29 @@ import cv2
 import numpy as np
 
 
-# Load our damaged photo
+# load gambar yang rusak
 image = cv2.imread('abraham.jpg')
 cv2.imshow('Original Damaged Photo', image)
 cv2.waitKey(0)
 
-# Load the photo where we've marked the damaged areas
+# load gambar yang sudah di mark bagian yang rusak
 marked_damages = cv2.imread('mask.jpg', 0)
 cv2.imshow('Marked Damages', marked_damages)
 cv2.waitKey(0)
 
-# Let's make a mask out of our marked image be changing all colors 
-# that are not white, to black
+# kasih threshold biar keliatan jelas bagian yang rusak
 ret, thresh1 = cv2.threshold(marked_damages, 254, 255, cv2.THRESH_BINARY)
 cv2.imshow('Threshold Binary', thresh1)
 cv2.waitKey(0)
 
-# Let's dilate (make thicker) our the marks w made
-# since thresholding has narrowed it slightly
+# gambar di dilate biar bagian yang rusak terlihat makin lebar
 kernel = np.ones((7,7), np.uint8)
 mask = cv2.dilate(thresh1, kernel, iterations = 1)
 cv2.imshow('Dilated Mask', mask)
 cv2.imwrite('images/abraham_mask.png', mask)
 
 cv2.waitKey(0)
+# fungsi utama yang digunakan untuk photo restoration
 restored = cv2.inpaint(image, mask, 3, cv2.INPAINT_TELEA)
 
 cv2.imshow('Restored', restored)
